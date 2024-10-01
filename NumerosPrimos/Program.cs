db82f3dade00135a656d2d5416558ecf7174a8dc
Sara Monteiro <saramonteiro2008@gmail.com>
2024-10-01 14:40:41 -0300

    NumerosPrimos

--- NumerosPrimos/Program.cs
+++ NumerosPrimos/Program.cs
@@ -1,17 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
-Console.WriteLine("Digite um algarismo para verificar se é um número primo: ");
-if(int.TryParse(Console.ReadLine(), out int numero))
+internal class Program
 {
-    int divisor;
-    if(numero < 2)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Este número não entra nesta classificação")
-    }
-    if(numero % divisor == 0)
-    {
-      
+        Console.WriteLine("Digite um algarismo para verificar se é um número primo: ");
+        if (int.TryParse(Console.ReadLine(), out int numero))
+        {
+            int divisor;
+            if (numero < 2)
+            {
+                Console.WriteLine("Este número não entra nesta classificação. ");
+            }
+            if (numero % divisor == 0)
+            {
+                Console.WriteLine("Este número não é primo. ");
+            }
+        }
     }
 }
-
